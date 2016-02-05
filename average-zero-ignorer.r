9b9229332a88d72f06a1305e5bb95f37f10a190d
Wayne <wayne-smart@users.noreply.github.com>
2016-02-05 09:31:02 +0100

    Create average-zero-ignorer.r

---
+++ average-zero-ignorer.r
@@ -0,0 +1,16 @@
+# script to calculate average of values while ignoring any zeros
+
+list <- c() # list of values to be averaged
+total <- 0
+months <- 0
+
+for (l in list) {
+    if (l == 0)
+        next
+    else (l != 0)
+        total <- total+l
+        months <- months+1
+}
+print(total)
+print(months)
+print(total / months)
